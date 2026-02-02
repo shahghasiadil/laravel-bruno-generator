d5224d7817f95afe2d69ef6fe3aa6b9c1fcab7cf
Shahghasi Adil <adil.shahghasi@gmail.com>
2026-02-02 21:22:32 +0430

    refactor: use factory pattern in BrunoSerializerService
    - Refactor BrunoSerializerService to use FormatSerializerFactory
    - Delegate format-specific logic to serializer implementations
    - Update CollectionMetadata to include output format
    - Determine file extensions based on selected format

--- src/DTO/CollectionMetadata.php
+++ src/DTO/CollectionMetadata.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace ShahGhasiAdil\LaravelBrunoGenerator\DTO;
 
+use ShahGhasiAdil\LaravelBrunoGenerator\Enums\OutputFormat;
+
 final readonly class CollectionMetadata
 {
     public function __construct(
         public string $name,
         public string $version,
         public string $type = 'collection',
+        public OutputFormat $format = OutputFormat::BRU,
     ) {}
 
     /**
